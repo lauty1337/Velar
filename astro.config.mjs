312584c657603842961b77efef94b26734f4ee0a
Lauty <lautarosanchezvisser@gmail.com>
2026-02-15 21:05:24 -0300

    definiendo outpot del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  output: 'static',
+
   vite: {
     plugins: [tailwindcss()],
     build: {
-      // Inline CSS menor a 4kb para reducir requests
       cssMinify: true,
-      // Mejor tree-shaking
       target: 'esnext',
-      // Inline assets pequeños
       assetsInlineLimit: 4096,
     },
   },
-  // Comprimir HTML
+
   compressHTML: true,
 })
